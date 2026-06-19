bdb569e52ece99b2331a60347fa90a9ea029484e
Enadiz76 <zidtimo@gmail.com>
2026-06-19 17:17:34 -0400

    Testing scroller, medium single image infinite scroller

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import './Landing.css'
 
 function Landing({ onNavigate }) {
   return (
     <section className="screen screen-landing">
-      <p className="screen-eyebrow">Landing screen</p>
-      <h1>Test Landing Screen</h1>
-      <p className="screen-copy">Click a navbar item or one of the buttons below to swap screens.</p>
-      <div className="screen-actions">
-        <button type="button" onClick={() => onNavigate('dev')}>
-          Open Dev
-        </button>
-        <button type="button" onClick={() => onNavigate('contact')}>
-          Open Contact
-        </button>
+      <div className="container">
+        <div className="background-element">
+          <p className="screen-eyebrow">Landing screen</p>
+          <h1>Test Landing Screen</h1>
+          <p className="screen-copy">Click a navbar item or one of the buttons below to swap screens.</p>
+          <div className='infinite-scrolling'></div>
+        </div>
+        {/* <div className="navigation-links"> */}
+          <div className="foreground-element">
+            <button type="button" onClick={() => onNavigate('dev')}>
+            Open Dev
+          </button>
+          <button type="button" onClick={() => onNavigate('contact')}>
+            Open Contact
+          </button>
+          </div>
+        {/* </div> */}
       </div>
     </section>
   )
